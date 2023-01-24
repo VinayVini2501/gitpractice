9dd91e6cd7e1dfbaa3541e0e90f6368a0666b1e2
vinay <vinayvini2501@gmail.com>
2023-01-24 14:16:27 +0530

    1st commit

---
+++ src/test/java/test/Edit.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Edit
+{
+	@Test(groups= {"Smoke"})
+	public void edited()
+	{
+		System.out.println("Complete editing");
+	}
+
+}
---
+++ src/test/java/test/delete.java
@@ -0,0 +1,10 @@
+package test;
+
+public class delete
+{
+	public void deleted()
+	{
+		System.out.println("deleted");
+	}
+
+}
